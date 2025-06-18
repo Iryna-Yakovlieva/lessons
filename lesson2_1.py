89aea4719ec409ec7048f867b7d6659c4696a6ea
Iryna-Yakovlieva <irviks@gmail.com>
2025-06-18 22:32:37 +0300

    ДЗ: Написати програму, яка просить користувача ввести 4-х значне число з клавіатури, після чого друкує на екрані стовпчик цифр, з якого це число складається.

--- lesson2_1.py
+++ lesson2_1.py
@@ -1,2 +1,6 @@
-user_input = input("Введіть 4 цифри: ")
-print(type(user_input))
+user_input = int(input("Введіть ціле 4-х значне число: "))
+n1 = user_input // 1000
+n2 = (user_input % 1000) // 100
+n3 = (user_input % 100) // 10
+n4 = user_input % 10
+print(n1, n2, n3, n4, sep='\n')
