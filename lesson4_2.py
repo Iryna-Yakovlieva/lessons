cfa39217419241273b86554318516ae32f517243
Iryna-Yakovlieva <irviks@gmail.com>
2025-06-24 20:43:30 +0300

    ДЗ 4.2. Знайти суму елементів із парними індексами

--- lesson4_2.py
+++ lesson4_2.py
@@ -6,4 +6,12 @@
 # [] => 0
 
 first_list = [0, 1, 7, 2, 4, 8]
-new_lst = first_list[1:]
+if len(first_list) == 0:
+    new_lst2 = 0
+else:
+    new_lst = first_list[::2]
+    sum_list = 0
+    for item in new_lst:
+        sum_list += item
+    new_lst2 = sum_list * first_list[-1]
+print(first_list,"->", new_lst2)
