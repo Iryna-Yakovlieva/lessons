9a3f698875eb67542a2b1f6a2d6d9e23e99e9e6f
Iryna-Yakovlieva <irviks@gmail.com>
2025-06-29 18:45:15 +0300

    ДЗ 5.1. Ім'я змінної

--- lesson5_1.py
+++ lesson5_1.py
@@ -7,40 +7,25 @@
 # При цьому повне ім'я змінної повино складатись не більш чим з одного нижнього підкреслення "_".
 # Список зареєстрованих слів можна взяти із keyword.kwlist.
 # У результаті перевірки на друк виводиться або True, якщо таке ім'я змінної допустимо, або False - якщо ні.
+# # Приклади імен змінних та результат перевірки (=> на друк виводити не потрібно :))
 
-# _ => True
-# __ => False
-# ___ => False
-# x => True
-# get_value => True
-# get value => False
-# get!value => False
-# some_super_puper_value => True
-# Get_value => False
-# get_Value => False
-# getValue => False
-# 3m => False
-# m3 => True
-# assert => False
-# assert_exception => True
 from string import punctuation
 import keyword
-
-user_input_string = "_"
+user_input_string = input("Введіть вираз: ")
 allowed_exceptions = "_"
 # Не починається з цифр
-string_no_numbers = not user_input_string.startswith(('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'))
-# Перший символ у верхньому, а решта у нижньому регістрі?
+string_no_numbers = not (user_input_string and user_input_string[0].isdigit())
+# Не містити великі літери
 string_no_register = not any(char.isupper() for char in user_input_string)
-# пробіл і знаки пунктуації
+# Змінна не може пробіл і знаки пунктуації, окрім нижнього підкреслення "_".
 string_no_character = all(
     char not in punctuation or char in allowed_exceptions
     for char in user_input_string
 )
 # бути жодним із зареєстрованих слів.
 string_no_keywords = user_input_string not in keyword.kwlist
-# При цьому повне ім'я змінної повино складатись не більш чим з одного нижнього підкреслення "_".
-max_text = user_input_string.count('_') <= 1
+# При цьому повне ім'я змінної повино складатись не більш чим з одного нижнього підкреслення "_". Уточнення підряд!!
+max_text = user_input_string.count('_' * 2) <= 0
 no_spaces = ' ' not in user_input_string
 if (
     string_no_register
