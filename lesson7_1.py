568141b64134af2cc3a7b6b35a57fd0affd79c81
Iryna-Yakovlieva <irviks@gmail.com>
2025-07-07 17:42:50 +0300

    ДЗ 7.1. Вітання

--- lesson7_1.py
+++ lesson7_1.py
@@ -4,5 +4,4 @@ def say_hi(name: str, age: int):
 assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
 assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
 print('ОК')
-
 print(say_hi("Alex", 32))
