f48e29f064963129ca744777073f9f856e2ce9cf
Iryna-Yakovlieva <irviks@gmail.com>
2025-07-13 12:01:46 +0300

    ДЗ 9.1. Визначити популярність певних слів у тексті

--- lesson9_1.py
+++ lesson9_1.py
@@ -1,8 +1,8 @@
 def popular_words (text: str, words: list):
-	text = text.lower().split()
-	word_counts = {}
-	for word in words:
-		word_counts[word] = text.count(word)
-	return word_counts
+    text = text.lower().split()
+    word_counts = {}
+    for word in words:
+        word_counts[word] = text.count(word)
+    return word_counts
 assert popular_words('''When I was One I had just begun When I was Two I was nearly new ''', ['i', 'was', 'three', 'near']) == { 'i': 4, 'was': 3, 'three': 0, 'near': 0 }, 'Test1'
 print('OK')
