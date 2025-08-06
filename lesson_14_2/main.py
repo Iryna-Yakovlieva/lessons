1e44893bc62dcbc97dac42e8112f52d9da70a2de
Iryna-Yakovlieva <irviks@gmail.com>
2025-08-06 20:45:20 +0300

    ДЗ 14.2. Створення власних модулів

--- lesson_14_2/main.py
+++ lesson_14_2/main.py
@@ -10,8 +10,6 @@ assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
 assert gr.find_student('Jobs2') is None, 'Test2'
 assert gr.find_student('Jobs') == st1
 assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод поиска должен возвращать экземпляр'
-
 gr.delete_student('Taylor')
 print(gr)  # Only one student
-
 gr.delete_student('Taylor')  # No error!
